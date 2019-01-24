111e9d3a46ee77df33c8fe24f7bb2cb7713b5f54
lev <laptevlew@yandex.ru>
2019-01-24 21:33:39 +0300

    условия

---
+++ lesson_8/script.js
@@ -0,0 +1,29 @@
+// if (2*4 == 8) {
+//     console.log('верно');
+// } else {
+//     console.log(' no верно');
+// }
+
+// let num = 50;
+
+// if (num < 49) {
+//     console.log('no верно');
+// } else if (num > 100) {
+//     console.log('много');
+// } else {
+//     console.log('верно');
+// };
+
+// (num == 50) ? console.log('верно') : console.log('no верно');
+
+// switch (num) {
+//     case num < 49:
+//         console.log('no верно');
+//         break;
+//     case num > 100:
+//         console.log('много');
+//         break;
+//      case 50:
+//         console.log('верно');
+//         break;
+// }
