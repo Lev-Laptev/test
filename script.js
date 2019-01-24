7548c57bf54906c99098ce63632407ac9f63da01
lev <laptevlew@yandex.ru>
2019-01-24 20:34:55 +0300

    это первый коммит

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// let arr = ['plum.png', 'orange.jgf', 'apple.svg'];
+
+// console.log(arr[0]);
+
+// alert('Hello World');
+// let answer = confirm('Are you here&');
+// console.log(answer);
+
+// let answer = +prompt('Are you here&', 'yes');
+// console.log(typeof(answer));
+
+// console.log(4 + '- object');
+
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(person['isMarried']);
+
+let incr = 10,
+decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
