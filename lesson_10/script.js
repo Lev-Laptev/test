48dbeb979b2110d03407cb35e81135b6508b0e6f
lev <laptevlew@yandex.ru>
2019-01-25 21:22:51 +0300

    function

---
+++ lesson_10/script.js
@@ -0,0 +1,42 @@
+// let num = 20;
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+    
+// showFirstMessage("make money lev");
+// console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(10, 60));
+
+console.log(calc(10, 632));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let aotNum = retVar();
+
+console.log(aotNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+
+let tw = "12.2px";
+
+
+// console.log(Math.round(tw));
+
+
+console.log(parseInt(tw));
+console.log(parseFloat(tw));
