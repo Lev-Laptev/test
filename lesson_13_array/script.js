3955f730c4be161d97501c731b1046abf92a6939
lev <laptevlew@yandex.ru>
2019-01-27 23:32:45 +0300

    array lesson 13

---
+++ lesson_13_array/script.js
@@ -0,0 +1,60 @@
+// let options = {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
+
+// console.log(options.name);
+// options.bool = false;
+// options.color = {
+//     border: "black",
+//     bg: "red",
+// };
+
+// delete options.bool;
+
+// console.log(options);
+
+
+// for (let key in options) {
+//     console.log("свойства" + key + " имеет значение" + options[key]);
+// }
+
+// console.log(Object.keys(options).length);
+
+// let arr = ["first", 2, 3, "four", 5];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// arr.pop();
+// arr.push("5");
+// arr.shift();
+// arr.unshift("1");
+
+// arr.forEach(function(item, i, mass){
+//     console.log(i + ": " + item + " (массив: " + mass + ')');
+// });
+// console.log(arr);
+
+// let mass = [1, 3, 4, 6, 7];
+// for (const key of mass) {
+//     console.log(key);
+// }
+
+// let ans = prompt("",""),
+//     arr = [];
+
+// arr = ans.split(',');
+// console.log(arr);
+
+// let arr = ["sfd","zzz","ds","df"],
+// i = arr.join(', ');
+// console.log(i);
+
+let arr = [1,15,4],
+i = arr.sort(compareNum);
+
+function compareNum(a,b) {
+    return a-b
+}
+console.log(arr);
