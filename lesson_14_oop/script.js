31b47f4d89e81d6a5e008e6f95f3685d56ebe2c1
lev <laptevlew@yandex.ru>
2019-02-05 22:52:31 +0300

    this

--- lesson_14_oop/script.js
+++ lesson_14_oop/script.js
@@ -1,47 +1,59 @@
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-
-//     this.hello = function() {
-//         console.log('hello' + this.name); 
-//     }
-// }
-// User.prototype.exit = function(name) {
-//     console.log('пользователь' + this.name + 'ушел');
-// }
-// let ivan = new User('Ivan', 25),
-//     alex = new User('Alex', 20);
-
-// console.log(alex);    
-// console.log(ivan);    
-
-// ivan.hello();
-// alex.exit();
-
-
-// es6
-
-class User {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
+'use strict'
+function showThis(a, b) {
+    console.log(this); 
+    function sum() {
+        console.log(this); 
+        return a + b;
     }
+    console.log(sum()); 
+}
 
-    hello() {
-        console.log('Hello ${this.name}')
-    }
-    exit() {
-        console.log('пользователь ${this.name} ушел')
+showThis(4,5);
+showThis(7,5);
+// 1) просто вызов функции - window/undefined
+
+let obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
+        console.log(this);
+        function shout() {
+            console.log(this);
+        }
+        shout();
     }
 }
 
-let ivan = new User('Ivan', 23);
-let alex = new User('Alex', 20);
+obj.sum();
 
-console.log(alex);    
-console.log(ivan);    
+// 2) метод обьекта - this = обьект
+// 3)  конструктор (new) - this = новый созданый обьект
 
-ivan.hello();
-alex.exit();
+let  user = {
+    name: "joe"
+}
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+
+console.log(sayName.call(user, 'Smit'));
+console.log(sayName.apply(user, ['Show']));
+
+function count(number) {
+    return this * number;
+}
+
+let double = count.bind(2);
+
+console.log(double(5));
+console.log(double(10));
+
+// 4) указание конкретного контекста - call apply bind
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+    console.log(this);
+    this.style.marginRight = '10px';
+});
