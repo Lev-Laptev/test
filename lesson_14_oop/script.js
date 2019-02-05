fd9524f228c12f02645140dd76fc30e58381ef58
lev <laptevlew@yandex.ru>
2019-02-05 22:08:49 +0300

    classes es 6 this constrc

--- lesson_14_oop/script.js
+++ lesson_14_oop/script.js
@@ -1,31 +1,47 @@
-// let timerId = setTimeout(sayHello, 3000);
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
 
-// clearTimeout(timerId);
-
-// let timerId = setInterval(sayHello, 3000);
-
-// clearTimeout(timerId); 
-
-// function sayHello(arams) {
-//    console.log('hello');
+//     this.hello = function() {
+//         console.log('hello' + this.name); 
+//     }
 // }
+// User.prototype.exit = function(name) {
+//     console.log('пользователь' + this.name + 'ушел');
+// }
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 20);
 
-// let timerId = setTimeout(function log() {
-//     console.log('hello');
-//     setTimeout(log, 2000);
-// });
+// console.log(alex);    
+// console.log(ivan);    
 
-// let btnBlock = document.querySelector('.block'),
-//     btns = document.getElementsByTagName('button');
+// ivan.hello();
+// alex.exit();
 
-// btnBlock.addEventListener("click",function (event) {
-//     if (event.target && event.target.tagName == 'BUTTON') {
-//         console.log('hello');
-//     } 
-// });
 
-// btnBlock.addEventListener("click",function (event) {
-//     if (event.target && event.target.matches('button.block')) {
-//         console.log('hello');
-//     } 
-// });
+// es6
+
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+
+    hello() {
+        console.log('Hello ${this.name}')
+    }
+    exit() {
+        console.log('пользователь ${this.name} ушел')
+    }
+}
+
+let ivan = new User('Ivan', 23);
+let alex = new User('Alex', 20);
+
+console.log(alex);    
+console.log(ivan);    
+
+ivan.hello();
+alex.exit();
